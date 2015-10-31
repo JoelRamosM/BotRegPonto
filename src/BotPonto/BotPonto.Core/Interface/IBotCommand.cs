f8cd37a9537732e03916caec906e4551147c0525
Joel <joel.r.mz@gmail.com>
2015-10-30 22:45:48 -0300

    Start

---
+++ src/BotPonto/BotPonto.Core/Interface/IBotCommand.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BotPonto.Core.Interface
+{
+    public interface IBotCommand
+    {
+        string Command { get; }
+        List<string> Parameters { get; }
+    }
+}
---
+++ src/BotPonto/BotPonto.Service.Console/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Web.Http;
+using BotPonto.Service.Console.Configuration;
+using Ninject;
+using Ninject.Web.Common.OwinHost;
+using Ninject.Web.WebApi.OwinHost;
+using Owin;
+
+namespace BotPonto.Service.Console
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var webApiConfiguration = ConfigureWebApi();
+            app.UseNinjectMiddleware(NinjectBootstraper.CreateKernel).UseNinjectWebApi(webApiConfiguration);
+        }
+
+        private HttpConfiguration ConfigureWebApi()
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+            return config;
+
+        }
+
+        private static StandardKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            return kernel;
+        }
+    }
+}
